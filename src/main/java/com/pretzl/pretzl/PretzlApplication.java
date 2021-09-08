208230097ecb43072ed8e6badfe33ee000f17e7c
Sadineni <bhaskar.sadineni@accenture.com>
2021-09-08 21:35:37 +0530

    Intial checkin

---
+++ src/main/java/com/pretzl/pretzl/PretzlApplication.java
@@ -0,0 +1,13 @@
+package com.pretzl.pretzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretzlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PretzlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pretzl/pretzl/PretzlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pretzl.pretzl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PretzlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
