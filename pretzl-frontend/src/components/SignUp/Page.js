619df9a25e39e536d3daecd1c8cd0f44e49845af
The-olajumoke <62016089+The-olajumoke@users.noreply.github.com>
2021-11-29 06:44:54 +0100

    edits

--- pretzl-frontend/src/components/SignUp/Page.js
+++ pretzl-frontend/src/components/SignUp/Page.js
@@ -4,7 +4,7 @@ import Img from "../../Exports/Back.svg";
 function Page({ children }) {
   return (
     <div className=" overlay ">
-      <img className=" overlayImg w-full h-24 ring" src={Img} alt="" />
+      <img className=" overlayImg w-full h-24 " src={Img} alt="" />
       <div className="  w-full  z-10">{children}</div>
     </div>
   );
