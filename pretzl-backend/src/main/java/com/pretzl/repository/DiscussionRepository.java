fade899a144595901968f0559d48b85373bc49c9
Sadineni <bhaskar.sadineni@accenture.com>
2022-01-15 11:16:17 +0530

    Guest login changes

--- pretzl-backend/src/main/java/com/pretzl/repository/DiscussionRepository.java
+++ pretzl-backend/src/main/java/com/pretzl/repository/DiscussionRepository.java
@@ -49,6 +49,6 @@ public interface DiscussionRepository extends JpaRepository<Discussion, Long> {
     @Query(value="select id as id,description as description FROM discussions where set_id in (SELECT id FROM discussions where username = :username and action_type='S') and (action_type ='P' or action_type='R')", nativeQuery = true)
     List<IThreads> getAllThreads(@Param("username") String username);
 
-    @Query(value="select * FROM discussions where set_id in (SELECT set_id FROM discussions where id = :discussionId and action_type='D')", nativeQuery = true)
+    @Query(value="select * FROM discussions where id = :discussionId and action_type='D' ", nativeQuery = true)
     List<Discussion> getUserDiscussionsById(String discussionId);
 }
